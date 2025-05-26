34323ad075dd9e976d5c037f0b6b7c169b48b8a3
Alla <allaabdulina705@gmail.com>
2025-05-26 16:13:44 +0500

    Lesson 3 End

---
+++ main.js
@@ -0,0 +1,32 @@
+// document.querySelector("#one").innerHTML = "<h1> anime </h1>"
+
+// let btn = document.querySelector(".check-in");
+// let inputIn = document.querySelector(".text");
+// let div = document.querySelector(".out");
+
+// btn.onclick = function () {
+//   console.log(inputIn.value)
+//   let e = inputIn.value
+
+//   div.innerHTML = e
+// }
+
+// let myHappy = prompt ("where are your birth bro ?")
+
+// if (myHappy == 22) {
+//   console.log("Yes")
+// } else if (myHappy <22) {
+//   console.log ("Меньше чем нужно")
+// } else {
+//   console.log ("No")
+// }
+
+let myHappy = prompt ("введите число")
+
+if (myHappy == 100) {
+  console.log("Yes")
+} else if (myHappy <100) {
+  alert ("Меньше чем нужно")
+} else  {
+  console.log ("Больше чем нужно")
+}
